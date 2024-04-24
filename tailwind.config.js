7ed854b42a8a8ae6a058e5b6877bf98796969f48
minikekb <minikekb@gmail.com>
2024-04-24 18:20:20 +0500

    Adding the first version of the header to the site

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,16 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        mainColor: '#e2e2e2'
+      },
+      padding: {
+        big: '50px'
+      },
+    },
+  },
 }
